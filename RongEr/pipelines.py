7e6e8447dee7e3667862e20a9f28c2f86f5fb8e3
YangBo <yang.wilby@gmail.com>
2016-09-13 17:46:55 +0200

    write to file as Chinese successful

--- RongEr/pipelines.py
+++ RongEr/pipelines.py
@@ -5,10 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import io
 class RongerPipeline(object):
     def __init__(self):
-        self.file = open('items.jl', 'wb')
+        self.file = io.open('items.json', 'w', encoding='utf8')
     def process_item(self, item, spider):
         line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
+        self.file.write(line.decode('unicode_escape'))
         return item
