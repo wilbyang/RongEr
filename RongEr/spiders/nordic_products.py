df0f303d1bac520cb25393ec994152a6672d0bb4
YangBo <yang.wilby@gmail.com>
2016-09-13 16:34:20 +0200

    first step success, but can not write file as Chinese

---
+++ RongEr/spiders/nordic_products.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from RongEr.items import RongerItem
+class NordicProductsSpider(CrawlSpider):
+    name = "nordic_products"
+    allowed_domains = ["livinorden.com"]
+    start_urls = (
+        'http://www.livinorden.com/adulthealproducts/adult_health_products.html',
+    )
+    rules = (
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=('adulthealproducts\/adulthealproducts\/.*\.html', )), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        item = RongerItem()
+        item['name'] = response.xpath("//div[@id='content']//h3/text()").extract()[0]
+        descr = response.xpath("//div[@id='content']//text()").extract()
+        item['description'] = ''.join(descr).encode("utf8")
+        item['image'] = response.xpath("//div[@id='content']//img/@src").extract()[0]
+        item['largeImage'] = response.xpath("//div[@id='content']//img/@src").extract()[0]
+        yield item
